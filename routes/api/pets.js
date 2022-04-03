501bf2d2e9a711c97844d1053eefc0f92cf32344
xlmriosx <xlmriosx@gmail.com>
2022-04-03 20:48:30 -0300

    Operations CRUD added for entity pet

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -2,9 +2,29 @@ const router = require('express').Router();
 
 const {Pet} = require('../../db');
 
-router.get('/', (req, res) => {
-    res.send('Request GET ok!');
+router.get('/', async (req, res) => { 
+    const pets = await Pet.findAll();
+    res.json(pets);
 
+});
+
+router.post('/', async (req, res) => {
+    const pet = await Pet.create(req.body);
+    res.json(pet);
+});
+
+router.put('/:petID', async (req, res) => {
+    await Pet.update(req.body, {
+        where: { id_pet: req.params.petID }
+    });
+    res.json({ succes: 'Modified aplicated.'})
+});
+
+router.delete('/:petID', async (req, res) => {
+    await Pet.destroy({
+        where: { id_pet: req.params.petID}
+    });
+    res.json({ succes: 'Pet eliminated.' })
 });
 
 module.exports = router;
