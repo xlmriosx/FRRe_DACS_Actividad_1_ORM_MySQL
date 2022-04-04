5639e6203f19f0a42a2f4a9f0eede24537fc3348
xlmriosx <xlmriosx@gmail.com>
2022-04-04 15:06:32 -0300

    CRUD Pet, Owner, Certifica - Model Medical, Report

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 
 const PetModel = require('./models/Pet');
 const Certificate_AdoptionModel = require('./models/Certificate_Adoption');
-
+const OwnerModel = require('./models/Owner');
+const ReportModel = require('./models/Report');
+const MedicalHistoricalModel = require('./models/Medical_Historical');
 
 const sequelize = new Sequelize('test', 'xlmriosx', 'root123!', {
     host: 'localhost',
@@ -11,6 +13,10 @@ const sequelize = new Sequelize('test', 'xlmriosx', 'root123!', {
 
 const Pet = PetModel(sequelize, Sequelize);
 const Certificate_Adoption = Certificate_AdoptionModel(sequelize, Sequelize);
+const Owner = OwnerModel(sequelize, Sequelize);
+const Report = ReportModel(sequelize, Sequelize);
+const MedicalHistorical = MedicalHistoricalModel(sequelize, Sequelize);
+
 
 sequelize.sync({ force: false })
 .then(() => {
@@ -20,10 +26,11 @@ sequelize.sync({ force: false })
 Pet.hasOne(Certificate_Adoption,  {foreignKey: 'pet'});
 //Certificate_Adoption.hasOne(Pet,  {foreignKey: 'pet'})
 
-
-
 module.exports = {
     Pet,
     Certificate_Adoption,
+    Owner,
+    Report,
+    MedicalHistorical,
     
 };
